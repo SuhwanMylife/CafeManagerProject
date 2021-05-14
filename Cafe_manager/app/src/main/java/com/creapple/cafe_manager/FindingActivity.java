c2754c7356d7b51fb2847ea67bf04b44cf9d0715
lch9809 <70652390+lch9809@users.noreply.github.com>
2021-05-14 20:06:21 +0900

    아이디 비밀번호 찾기 기능
    아이디 비밀번호 찾기 기능 구현

--- Cafe_manager/app/src/main/java/com/creapple/cafe_manager/FindingActivity.java
+++ Cafe_manager/app/src/main/java/com/creapple/cafe_manager/FindingActivity.java
@@ -1,13 +1,15 @@
 package com.creapple.cafe_manager;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,7 +21,6 @@ import org.json.JSONObject;
 
 public class FindingActivity extends AppCompatActivity {
     Button btn_find_id, btn_find_pw;
-    String tem_pw;
     private EditText et_num_id, et_store_id, et_id_pw, et_num_pw;
 
     @Override
@@ -37,12 +38,17 @@ public class FindingActivity extends AppCompatActivity {
         btn_find_id.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //int find_num = Integer.parseInt(et_num_id.getText().toString());
+                String find_num = et_num_id.getText().toString();
                 String find_store = et_store_id.getText().toString();
 
-                if (find_store.length() == 0) {
+                if (find_store.equals("")) {
                     Toast.makeText(FindingActivity.this, "매장명을 입력해 주세요", Toast.LENGTH_SHORT).show();
                     et_store_id.requestFocus();
+                    return;
+                }
+                if (find_num.equals("")) {
+                    Toast.makeText(FindingActivity.this, "전화번호룰 입력해 주세요", Toast.LENGTH_SHORT).show();
+                    et_num_id.requestFocus();
                     return;
                 }
 
@@ -53,7 +59,14 @@ public class FindingActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(response);
                             boolean success = jsonObject.getBoolean("success");
                             if (success) {
+                                String userId = jsonObject.getString("userID");
                                 Toast.makeText(getApplicationContext(), "사용자가 있습니다", Toast.LENGTH_SHORT).show();
+                                new AlertDialog.Builder(FindingActivity.this)
+                                        .setTitle("당신의 아이디는")
+                                        .setMessage(userId+"\n입니다.")
+                                        .setPositiveButton("확인", null)
+                                        .show();
+
                             } else {
                                 Toast.makeText(getApplicationContext(), "해당 정보에 맞는 사용자가 없습니다", Toast.LENGTH_SHORT).show();
                             }
@@ -63,7 +76,7 @@ public class FindingActivity extends AppCompatActivity {
                         }
                     }
                 };
-                FindingRequest findingRequest = new FindingRequest(find_store, responseListener); //담아줌 넘 빼고
+                FindingRequest findingRequest = new FindingRequest(find_store, find_num, responseListener); //담아줌 넘 빼고
                 RequestQueue queue = Volley.newRequestQueue(FindingActivity.this);
                 queue.add(findingRequest);
             }
@@ -75,6 +88,7 @@ public class FindingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String find_id_pw = et_id_pw.getText().toString();
                 String find_num_pw = et_num_pw.getText().toString();
+
                 if (find_id_pw.equals("")) {
                     Toast.makeText(FindingActivity.this, "아이디룰 입력해 주세요", Toast.LENGTH_SHORT).show();
                     et_id_pw.requestFocus();
@@ -85,12 +99,35 @@ public class FindingActivity extends AppCompatActivity {
                     et_num_pw.requestFocus();
                     return;
                 }
-                tem_pw = "qwer1234"; //임시 비밀번호, 비밀번호 찾아서 지정하는 형식
-                new AlertDialog.Builder(FindingActivity.this)
-                        .setTitle("당신의 비밀번호는")
-                        .setMessage(tem_pw + "\n입니다")
-                        .setPositiveButton("확인", null)
-                        .show();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                String userPassword = jsonObject.getString("userPassword");
+                                Toast.makeText(getApplicationContext(), "사용자가 있습니다", Toast.LENGTH_SHORT).show();
+                                new AlertDialog.Builder(FindingActivity.this)
+                                        .setTitle("당신의 비밀번호는")
+                                        .setMessage(userPassword+"\n입니다.")
+                                        .setPositiveButton("확인", null)
+                                        .show();
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "해당 정보에 맞는 사용자가 없습니다", Toast.LENGTH_SHORT).show();
+                            }
+                            return;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                FindingPwRequest findingPwRequest = new FindingPwRequest(find_id_pw, find_num_pw, responseListener); //담아줌 넘 빼고
+                RequestQueue queue = Volley.newRequestQueue(FindingActivity.this);
+                queue.add(findingPwRequest);
             }
         });
     }
