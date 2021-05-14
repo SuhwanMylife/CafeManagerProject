8797bb237c070c1b11f2b8a19669ddb8ec8607c1
lch9809 <70652390+lch9809@users.noreply.github.com>
2021-05-14 20:07:02 +0900

    아이디 비밀번호 찾기 기능
    아이디 비밀번호 찾기 기능 구현

--- Cafe_manager/app/src/main/java/com/creapple/cafe_manager/FindingRequest.java
+++ Cafe_manager/app/src/main/java/com/creapple/cafe_manager/FindingRequest.java
@@ -12,12 +12,12 @@ public class FindingRequest extends StringRequest {
     final static private String URL = "http://chandroid.dothome.co.kr/FindingID.php";
     private Map<String,String> map;
 
-    public FindingRequest( String userStore, Response.Listener<String> listener) {
+    public FindingRequest( String userStore, String userNumber, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
-       // map.put("userNumber", userNumber + ""); int userNumber,
         map.put("userStore", userStore);
+        map.put("userNumber", userNumber);
     }
     @Override
     protected Map<String, String> getParams() throws AuthFailureError {
