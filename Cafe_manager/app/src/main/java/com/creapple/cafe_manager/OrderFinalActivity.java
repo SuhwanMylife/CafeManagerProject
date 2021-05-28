d28a2a5c1e692d4b0fc72dce6d1f87b24421a79a
mylife3904@kakao.com <hagcheol0722->
2021-05-28 23:06:46 +0900

    발주 - 넘겨받은 발주품목을 `order` db에 저장

--- Cafe_manager/app/src/main/java/com/creapple/cafe_manager/OrderFinalActivity.java
+++ Cafe_manager/app/src/main/java/com/creapple/cafe_manager/OrderFinalActivity.java
@@ -1,9 +1,12 @@
 package com.creapple.cafe_manager;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,9 +15,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class OrderFinalActivity extends AppCompatActivity {
+
+    private static String SERVER_ADDRESS = "http://ghtjd8264.dothome.co.kr";
+    private static String TAG = "phpexample";
 
     private RecyclerView mRecyclerView;
     private OrderUserAdapter mAdapter;
@@ -71,6 +83,22 @@ public class OrderFinalActivity extends AppCompatActivity {
                 builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
+                        for (int i = 0; i < mArrayList.size(); i++) {
+                            int stock_num = Integer.parseInt(String.valueOf(mArrayList.get(i).getMember_pdt_stock_num()));
+                            if (stock_num != 0) {
+
+                                String pdt_name = mArrayList.get(i).getMember_pdt_name();
+                                String pdt_classification = mArrayList.get(i).getMember_pdt_unit();
+                                String pdt_unit = mArrayList.get(i).getMember_pdt_price();
+                                String pdt_price = mArrayList.get(i).getMember_pdt_stock();
+                                String pdt_stock_num = mArrayList.get(i).getMember_pdt_stock_num();
+
+                                OrderFinalActivity.InsertData task = new OrderFinalActivity.InsertData();
+                                task.execute( SERVER_ADDRESS + "/order_insert.php", pdt_name, pdt_classification, pdt_unit, pdt_price, pdt_stock_num);
+                            }
+                        }
+
                         Toast.makeText(OrderFinalActivity.this, "발주 신청이 성공적으로 이루어졌습니다.", Toast.LENGTH_LONG).show();
                         Intent intent = new Intent(OrderFinalActivity.this, OrderMainActivity.class);
                         startActivity(intent);
@@ -82,4 +110,93 @@ public class OrderFinalActivity extends AppCompatActivity {
         });
     }
 
+    class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(OrderFinalActivity.this, "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            // mTextViewResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String pdt_name = (String)params[1];
+            String pdt_classification = (String)params[2];
+            String pdt_unit = (String)params[3];
+            String pdt_price = (String)params[4];
+            String pdt_stock_num = (String)params[5];
+
+            String serverURL = (String)params[0];
+            String postParameters = "pdt_name=" + pdt_name + "&pdt_classification=" + pdt_classification + "&pdt_unit=" + pdt_unit + "&pdt_price=" + pdt_price + "&pdt_stock_num=" + pdt_stock_num;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
 }
