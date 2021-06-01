fa77127c17a1141244fa59c96382514d47b0fe21
lch9809 <70652390+lch9809@users.noreply.github.com>
2021-06-01 12:36:09 +0900

    색상 추가 4
    색상 추가 4

--- Cafe_manager/app/src/main/java/com/creapple/cafe_manager/SettingInfoActivity.java
+++ Cafe_manager/app/src/main/java/com/creapple/cafe_manager/SettingInfoActivity.java
@@ -3,14 +3,31 @@ package com.creapple.cafe_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SettingInfoActivity extends AppCompatActivity {
 
+    //추가(6.1)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_info);
+
+        //추가(6.1)
+        getSupportActionBar().setTitle("회사 정보");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
