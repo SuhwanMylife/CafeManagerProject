063cf80118c3f0adc74a035fc3c9b6213ed29cf0
lch9809 <70652390+lch9809@users.noreply.github.com>
2021-06-01 12:36:37 +0900

    색상 추가 4
    색상 추가 4

--- Cafe_manager/app/src/main/java/com/creapple/cafe_manager/SettingNoticeActivity.java
+++ Cafe_manager/app/src/main/java/com/creapple/cafe_manager/SettingNoticeActivity.java
@@ -3,12 +3,29 @@ package com.creapple.cafe_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingNoticeActivity extends AppCompatActivity {
+
+    //추가(6.1)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_notice);
+
+        //추가(6.1)
+        getSupportActionBar().setTitle("공지사항");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
