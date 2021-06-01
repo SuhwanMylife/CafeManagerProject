f5c91c9843dbdaefdb3ccaddaf9ae4613f05550a
TRASALBY <ghtjd8264@naver.com>
2021-06-01 16:57:15 +0900

    import menuitem
    액션바관련해서 import 안된 내용인듯

--- Cafe_manager/app/src/main/java/com/creapple/cafe_manager/RegisterActivity.java
+++ Cafe_manager/app/src/main/java/com/creapple/cafe_manager/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.creapple.cafe_manager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
